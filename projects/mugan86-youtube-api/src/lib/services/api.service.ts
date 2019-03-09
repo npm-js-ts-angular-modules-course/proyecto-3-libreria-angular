4e67b6ed924734517f5984a496dd426a7b789db4
Anartz Mugika <mugan86@gmail.com>
2019-03-09 19:28:00 +0100

    Resolve modal issue in dismiss

--- projects/mugan86-youtube-api/src/lib/services/api.service.ts
+++ projects/mugan86-youtube-api/src/lib/services/api.service.ts
@@ -5,5 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
+  // Canal info
+  // /channels?part=id%2Csnippet%2CcontentDetails&id=UCTh7-deUJBNv2tHRiMGcXxg&key=
+  // PLay list de usuario X
+  // /playlists?part=id%2Csnippet%2CcontentDetails&channelId=UCTh7-deUJBNv2tHRiMGcXxg&pageToken=CAUQAA&key=[YOUR_API_KEY]
   constructor() { }
 }
