b51de5e3216c6215658838998119f66f565f1f58
Anartz Mugika <mugan86@gmail.com>
2019-03-11 10:36:46 +0100

    Start to define user channel info

--- projects/mugan86-youtube-api/src/lib/services/api.service.ts
+++ projects/mugan86-youtube-api/src/lib/services/api.service.ts
@@ -1,3 +1,8 @@
+import { Channel } from './../interfaces/api/channel.interface';
+import { map } from 'rxjs/operators';
+import { API } from './../constants/urls';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,5 +14,22 @@ export class ApiService {
   // /channels?part=id%2Csnippet%2CcontentDetails&id=UCTh7-deUJBNv2tHRiMGcXxg&key=
   // PLay list de usuario X
   // /playlists?part=id%2Csnippet%2CcontentDetails&channelId=UCTh7-deUJBNv2tHRiMGcXxg&pageToken=CAUQAA&key=[YOUR_API_KEY]
-  constructor() { }
+  // Items de un play list
+  // playlistItems?part=id%2Csnippet%2CcontentDetails&playlistId=PLaaTcPGicjqiyuOFrie6fXVyUGJNp9yFe&key=[YOUR_API_KEY]
+  constructor(private config: ConfigService, private http: HttpClient) { }
+
+  private getUrl(request: string): string {
+    return `${API}${ request }&maxResults=9&key=${this.config.apiKey}`;
+  }
+
+  getUserChannelInfo( user: string ) {
+    return this.http.get(this.getUrl(`channels?part=id%2Csnippet%2CcontentDetails&id=${ user }`)).pipe(
+      map(
+        ( res: Channel ) => {
+          return res.items[0].snippet;
+        }
+      )
+    );
+  }
+
 }
