b1c5231f9125c6a38a8d9fbd4405720dbea7b4ee
Anartz Mugika <mugan86@gmail.com>
2019-03-11 10:36:57 +0100

    Get inject config info

--- projects/mugan86-youtube-api/src/lib/services/config.service.ts
+++ projects/mugan86-youtube-api/src/lib/services/config.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Config } from './../interfaces/config';
+import { Injectable, Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-
-  constructor() { }
+  apiKey: string;
+  constructor(@Inject('config') private config: Config) {
+    this.apiKey = this.config.apiKey;
+    if (this.config.showLog) {
+      console.log('Api key es: ', this.apiKey);
+    }
+  }
 }
